f36dbd5e253d1771011c26f4a9642a61e1a5ea63
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-09-22 22:12:51 +0530

    backend link

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 await connectDB();
 await connectCloudinary();
-const allowedOrigins = ["http://localhost:5173", "https://new-ecommerce-blue.vercel.app"];
+const allowedOrigins = ["http://localhost:5173", "https://new-ecommerce-blue.vercel.app", "https://new-ecommerce-backend-amits-projects-c91f797f.vercel.app"];
 app.get('/', (req, res) => res.send("API is Working"));
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
