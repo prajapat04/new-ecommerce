92b776eb5bc6df5fbe3b21912b14af6a7c3e98e9
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-10-24 17:04:34 +0530

    Refactor cookie settings to use environment variables

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -22,13 +22,14 @@ export const registerUser = async (req, res) => {
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction ? 'none' : 'lax',
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      path: '/'
-    });
+   res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  path: "/",
+});
+
     return res.json({
       success: true,
       user: {
@@ -64,13 +65,14 @@ export const loginUser = async (req, res) => {
       expiresIn: "7d",
     });
 
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction ? "none" : "Lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      path: '/'
-    });
+   res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  path: "/",
+});
+
 
     res.json({
       message: "Login in seccessfully",
